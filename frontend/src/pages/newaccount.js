d8c70200878ca49bf749756ea8259c9dc65bc5f9
Nithin <nsingha2@ucsc.edu>
2022-11-09 20:40:30 -0800

    cleanup comments and imports

--- frontend/src/pages/newaccount.js
+++ frontend/src/pages/newaccount.js
@@ -1,31 +1,11 @@
 import React, {useContext} from "react";
 import styles from '../styles.module.css'
-import { Button, Grid, Paper, Card, Alert, AlertTitle } from '@mui/material';
-import SignIn from './signIn.js'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-// import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-// import CloseIcon from '@mui/icons-material/Close';
-import IconButton from '@mui/material/IconButton';
-// import CategoryContext from './CategoryContext';
-// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Box from '@mui/material/Box';
-// import styles from '../styles.module.css'
-// import {useNavigate} from 'react-router-dom';
-// import {BrowserRouter as Router} from 'react-router-dom';
+import { Button, Alert, AlertTitle, AppBar, Toolbar, Typography, TextField, IconButton, Box } from '@mui/material';
 import useRouter from 'next/router';
-// import Router from "react-router-dom";
-import bcrypt from 'bcryptjs';
 
 const Register = () => {
-  // updates state for displaying eror when email/password is incorrect
-  // used to update the log out button to appear after logging in
-  // const {setVisible} = useContext(CategoryContext);
-  // used to go back to the main home page and goto new account page
-  // const history = useNavigate();
   const router = useRouter;
+  // conditionally display error if login credentials were invalid
   const [showError, setShowError] = React.useState(false);
   let user = {username: '', password: ''}
   const handleInputChange = (event) => {
