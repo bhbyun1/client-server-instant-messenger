3795c6fc9186d9cc2d6362417952c93883a307f6
Nithin <nsingha2@ucsc.edu>
2022-11-20 14:44:22 -0800

    registering account automatically logs you in as that user

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -35,7 +35,6 @@ const Login = () => {
     fetch(configData.HOSTNAME + ":5000/login", {
       headers: headers,
       method: 'POST',
-      body: JSON.stringify({ 'username': username, 'password': password })
     }).then((response) => response.json())
     .then((response) => {
       if (response.token) {
