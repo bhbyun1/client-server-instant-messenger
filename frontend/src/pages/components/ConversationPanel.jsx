ce7e7c9112b0e53536ff352712ae7cbbf060f3b4
Nithin <nsingha2@ucsc.edu>
2022-11-21 15:38:28 -0800

    removing old chat selector, code cleanup

--- frontend/src/pages/components/ConversationPanel.jsx
+++ frontend/src/pages/components/ConversationPanel.jsx
@@ -10,25 +10,16 @@ function ConversationPanel({setConversationMessages}) {
     //                   "Claire": [{'username': 'Claire', 'message': 'I\'m Claire'}]};
     const [messages, setMessages] = React.useState([]);
 
-    const handleAutocomplete = (event, value) => {
-        // setUsers(value);
-        // fetch endpoint for <id> chat history
-        //setMessages(mockData[value]);
-        sessionStorage.currentChat = value['id'];
-        console.log("handling autocomplete");
-        
-        //console.log(event);
-        console.log(value);
-        console.log("value");
+    const fetchConversationHistory = (chat) => {
+        sessionStorage.currentChat = chat['id'];
 
-        // call fetch chatid history api here instead of the above setmessages, using value["id"]
-
+        // Get message history from backend, then save it in state
         let headers = new Headers();
         headers.set('Content-Type', 'application/json');
         headers.set('Accept', 'application/json');
         headers.set('x-access-token', sessionStorage.token);
         
-        fetch(configData.HOSTNAME + ':5000/chat/' + value["id"], {
+        fetch(configData.HOSTNAME + ':5000/chat/' + chat['id'], {
         headers: headers,
         method: 'GET',
         }).then((response) => 
@@ -80,8 +71,10 @@ function ConversationPanel({setConversationMessages}) {
                     //console.log("chats:");
                     //console.log(chats);
                 }
+
+                // Set chat to general chat if nothing is currently selected
                 if (!sessionStorage.currentChat) {
-                    handleAutocomplete(null, displayChatroom[0]);
+                    fetchConversationHistory(displayChatroom[0]);
                 }
 
                 return response["chatrooms"];
@@ -89,78 +82,7 @@ function ConversationPanel({setConversationMessages}) {
                 console.log("couldn't fetch chatrooms");
             }
         });
-        //let chatroomList = response['chatrooms'];
-
-        //let chatroomList = accurateMockData["chatrooms"];
     });
-
-    const handleAutocomplete = (event, value) => {
-        // setUsers(value);
-        // fetch endpoint for <id> chat history
-        //setMessages(mockData[value]);
-        sessionStorage.currentChat = value['id'];
-        console.log("handling autocomplete");
-        
-        //console.log(event);
-        console.log(value);
-        console.log("value");
-
-        // call fetch chatid history api here instead of the above setmessages, using value["id"]
-
-        let headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        headers.set('Accept', 'application/json');
-        headers.set('x-access-token', sessionStorage.token);
-        
-        fetch(configData.HOSTNAME + ":5000/chat/" + value["id"], {
-        headers: headers,
-        method: 'GET',
-        }).then((response) => 
-        {
-            //console.log(response.text());
-            return response.json();
-        })
-        .then((response) => {
-            console.log("response text:");
-            console.log(response);
-            if (response) {
-                setConversationMessages(response['messages']); // probably need to edit this, i dont know the shape of the data
-                return response;
-            } else {
-                console.log("error fetching message history");
-            }
-        });
-    }
-
-    const handleChatSelect = (chat) => {
-        sessionStorage.currentChat = chat['id'];
-
-        // call fetch chatid history api here instead of the above setmessages, using value["id"]
-
-        let headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        headers.set('Accept', 'application/json');
-        headers.set('x-access-token', sessionStorage.token);
-        
-        fetch(configData.HOSTNAME + ":5000/chat/" + chat["id"], {
-        headers: headers,
-        method: 'GET',
-        }).then((response) => 
-        {
-            //console.log(response.text());
-            return response.json();
-        })
-        .then((response) => {
-            console.log("response text:");
-            console.log(response);
-            if (response) {
-                setConversationMessages(response['messages']); // probably need to edit this, i dont know the shape of the data
-                return response;
-            } else {
-                console.log("error fetching message history");
-            }
-        });
-    }
 
     return(
         <Box sx={{
@@ -169,18 +91,11 @@ function ConversationPanel({setConversationMessages}) {
             '& > *': { 
                 m: 1,}, 
         }}>
-            <Autocomplete
-                options={chats}
-                fullWidth
-                onChange={handleAutocomplete}
-                renderInput={(params) => <TextField {...params} variant="filled" label="Chats"/>}
-            />
         <List>
             {
                 chats.map((chat) => {
-                    console.log(chat)
                     return (
-                        <ListItem button divider onClick={() => handleChatSelect(chat)}>
+                        <ListItem button divider onClick={() => fetchConversationHistory(chat)}>
                             <ListItemText primary={chat.label} />
                         </ListItem>
                     )
