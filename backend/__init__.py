5d839c727a1a73858f9c7552a9f39086e3c6b03b
Jesse Rodarte <jerodarte@gmail.com>
2022-12-05 12:21:03 -0800

    cors fix

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,7 +2,6 @@
 from os import environ
 
 from dotenv import load_dotenv
-from flask_cors import CORS
 
 from .app import app, socketio
 from .routes import *
@@ -13,7 +12,6 @@ load_dotenv()
 
 def create_app(test_config=None):
     """Flask application factory."""
-    CORS(app, resources={r"/*": {"origins": "*"}})
     socketio.run(app,
                  debug=True,
                  host=environ.get('APP_HOST'),
