8d027b5e3e91c2eec9fb7740d0acbaeab05899cf
Jesse Rodarte <jerodarte@gmail.com>
2022-12-05 12:00:35 -0800

    quick cors fix

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@
 from os import environ
 
 from dotenv import load_dotenv
-
+from flask_cors import CORS
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
@@ -12,5 +12,6 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET'] = environ.get('API_TOKEN')
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('CONN_STR')
+CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 db = SQLAlchemy(app)
